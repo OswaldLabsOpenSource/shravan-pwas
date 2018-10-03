6286ef86a127ea5256197a6edb08432639f38c45
Anand Chowdhary <anandchowdhary@gmail.com>
2018-10-03 22:48:26 +0200

    Add location

--- app/components/App.js
+++ app/components/App.js
@@ -1,21 +1,42 @@
+const geolocation = require("nativescript-geolocation");
 const Counter = require('./Counter');
 
 module.exports = {
   data() {
     return {
-      surprise: false,
+      loading: false,
+      answer: ""
     };
+  },
+  methods: {
+    location() {
+      this.loading = true;
+      geolocation.enableLocationRequest();
+      geolocation.getCurrentLocation({ desiredAccuracy: "Accuracy.high", maximumAge: 5000, timeout: 20000 }).then(r => {
+        fetch(`https://platform.oswaldlabs.com/geocode/${r.latitude}/${r.longitude}`).then(r => r.json()).then(r => {
+        this.loading = false;
+        if (r.results.length) {
+          this.answer = r.results[0].formatted_address;
+        } else {
+          this.answer = "An error occurred in finding your location";
+        }
+      });
+      }).catch(() => {
+        this.answer = "An error occurred in finding your GPS location";
+      })
+    }
   },
   template: `
     <Page class="page">
-      <ActionBar class="action-bar" title="NativeScript-Vue"/>
+      <ActionBar class="action-bar" title="Visib11y" />
   
       <StackLayout>
       
-        <Label class="p-20" textWrap=true text="This is a hello world application, tap the button if you dare"/>
+        <Label class="p-20" textWrap=true text="Click on the button below to find out your current location." />
       
-        <Button class="btn btn-primary" @tap="surprise = !surprise" text="Tap me!"/>
-        <Image v-if="surprise" class="m-20" src="~/images/NativeScript-Vue.png"/>
+        <Button class="btn btn-primary" @tap="location" text="Where am I?" />
+        
+        <Label class="p-20" textWrap=true :text="loading ? 'Loading...' : answer" />
       </StackLayout>
     </Page>
   `,
