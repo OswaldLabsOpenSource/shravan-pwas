36e1d2bd9aff5e9df2036b55b36f98deb0d7422f
Anand Chowdhary <anandchowdhary@gmail.com>
2018-10-10 10:32:08 +0200

    Add live subtitles app

---
+++ live-subtitles/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLocationArrow, faMap, faEnvelopeOpenText, faCubes, faSearchPlus, faChevronLeft, faCamera, faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faLocationArrow, faMap, faEnvelopeOpenText, faCubes, faSearchPlus, faChevronLeft, faCamera, faSyncAlt)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+import Vue2TouchEvents from 'vue2-touch-events'
+Vue.use(Vue2TouchEvents)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
